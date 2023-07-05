9640c857481ba1a7c67c94ceeb4d0220d5d293a5
qooba <dev@qooba.net>
2023-07-05 23:13:44 +0200

    add ner tests

--- tests/integration_ner_anonymizer.rs
+++ tests/integration_ner_anonymizer.rs
@@ -1,24 +1,14 @@
-use std::{collections::HashMap, fs::File, io::BufReader};
-
 use anonymize_rs::{
-    anonymizer::ner_anonymizer::NerAnonymizer,
+    anonymizer::{ner_anonymizer::NerAnonymizer, ReplaceResult},
     config::{AnonymizePipelineConfig, AnonymizerConfig},
 };
 use anyhow::Result;
-use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize)]
-struct Config {
-    id2label: HashMap<String, String>,
-}
+async fn replace_with_ner(text: &str, model_name: &str, lang: &str) -> Result<ReplaceResult> {
+    let model_path = format!("./examples/{model_name}/model.onnx").to_string();
+    let tokenizer_path = format!("./examples/{model_name}/tokenizer.json").to_string();
 
-#[tokio::main]
-#[test]
-async fn test_ner_replace() -> Result<()> {
-    let model_path = "./examples/clarin-pl/model.onnx".to_string();
-    let tokenizer_path = "./examples/clarin-pl/tokenizer.json".to_string();
-
-    let path = "./tests/config/config_ner.yaml".to_string();
+    let path = format!("./tests/config/config_ner_{lang}.yaml").to_string();
     let config = AnonymizePipelineConfig::new(&path).await?;
     let id2label = if let AnonymizerConfig::Ner {
         model_path: _,
@@ -35,10 +25,36 @@ async fn test_ner_replace() -> Result<()> {
     let ner_anonymizer =
         NerAnonymizer::new(model_path, tokenizer_path, id2label.clone(), Some(true))?;
 
-    let text =
-        "Jan Kowalski mieszka w Krakowie na ulicy Warszawskiej. Jego numer telefonu to 555555555.";
-
     let res = ner_anonymizer.replace_matches(text, None)?;
+
+    Ok(res)
+}
+
+#[ignore]
+#[tokio::main]
+#[test]
+async fn test_ner_replace_pl() -> Result<()> {
+    let text =
+        "Jan Kowalski i Anna Kowalska mieszka w Krakowie na ulicy Warszawskiej. Jego numer telefonu to 555555555.";
+
+    let model_name = "clarin-pl";
+    let lang = "pl";
+    let res = replace_with_ner(text, model_name, lang).await?;
+    println!("{:?}", res);
+
+    //assert_eq!(res.text, "I like to eat FRUIT0 and FRUIT1 and FRUIT2");
+    Ok(())
+}
+
+#[ignore]
+#[tokio::main]
+#[test]
+async fn test_ner_replace_en() -> Result<()> {
+    let text = "My name is Sarah and I live in London";
+
+    let model_name = "dslim";
+    let lang = "en";
+    let res = replace_with_ner(text, model_name, lang).await?;
     println!("{:?}", res);
 
     //assert_eq!(res.text, "I like to eat FRUIT0 and FRUIT1 and FRUIT2");
